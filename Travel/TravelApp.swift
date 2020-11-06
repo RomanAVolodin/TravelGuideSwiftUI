843025a5f80fd3958f923046de150b6c01a00db8
Roman Volodin <32717824+RomanAVolodin@users.noreply.github.com>
2020-11-06 10:17:47 +0300

    Initial Commit

---
+++ Travel/TravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelApp.swift
+//  Travel
+//
+//  Created by Roman Vol on 06.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct TravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
